08e6cbf33220886c7c502f1b6872f3fbdfa6c427
usefulsplash <usefulsplash@proton.me>
2025-02-09 22:32:06 +0000

    change alpha shortcuts

--- config.def.h
+++ config.def.h
@@ -208,9 +208,9 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ MODKEY,               XK_bracketleft, chgalpha,       {.f = -1} }, /* Decrease opacity */
-	{ MODKEY|ShiftMask,     XK_braceright,  chgalpha,       {.f = +1} }, /* Increase opacity */
-	{ MODKEY,               XK_bracketright,chgalpha,       {.f =  0} }, /* Reset opacity */
+	{ MODKEY,               XK_a,		chgalpha,       {.f = -1} }, /* Decrease opacity */
+	{ MODKEY|ShiftMask,     XK_s,  		chgalpha,       {.f = +1} }, /* Increase opacity */
+	{ MODKEY,               XK_d,		chgalpha,       {.f =  0} }, /* Reset opacity */
 };
 
 /*
