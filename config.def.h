770549b67809887126c6c844abb90f7d23d63c83
usefulsplash <usefulsplash@proton.me>
2025-02-09 20:22:38 +0000

    change font size

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Victor Mono Nerd Font:pixelsize=17:antialias=true:autohint=true";
+static char *font = "Victor Mono Nerd Font:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 16;
 
 /*
