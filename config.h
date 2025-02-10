92e253df1a147e12b670c2db10acecbbf3f6c589
usefulsplash <usefulsplash@proton.me>
2025-02-10 18:25:43 +0000

    hail mary color changes PLEASE WORK

--- config.h
+++ config.h
@@ -134,8 +134,8 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
 unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
